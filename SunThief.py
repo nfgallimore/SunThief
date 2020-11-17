b56ac0d80903c98698d5e28ca3ca65a58014e44f
Nick Gallimore <nicholas.gallimore@rovisys.com>
2020-11-17 11:40:06 -0500

    Add config settings

--- SunThief.py
+++ SunThief.py
@@ -1,30 +1,54 @@
-import time, sched, json, requests
+import time, sched, json, requests, json
 
-def sun_thief():
-	auth = {'Authorization': 'insert_token_here'}
+def get_config():
+	global config
 
-	wcb = requests.get('https://discord.com/api/v8/channels/770027870876860466/messages?limit=10', headers=auth).json()
-	versus = requests.get('https://discord.com/api/v8/channels/778139603966230548/messages', headers=auth).json()
+	file = open("config.json", "r")
+	config = json.loads(file.read())
+	file.close()
 
-	wcbPosts = list(map(lambda post: f'**{post["author"]["username"]}**\n{post["content"]}', wcb))
-	versusPosts = list(map(lambda post: post["content"], versus))
-	newPosts = list(set(wcbPosts) - set(versusPosts))
+def post():
+	auth = {'Authorization': config["token"]}
 
-	for post in wcb:
-		if f'**{post["author"]["username"]}**\n{post["content"]}' in newPosts:
-			content = f'**{post["author"]["username"]}**\n{post["content"]}'
-			resp = requests.post('https://discordapp.com/api/webhooks/778139913022734356/bMil0rT6KRy_8KAm3_YTpQIpgzKjNFV4ouYLC9H72sQSG5hHb_ZfiVKQkOipLfgBGPSx', headers={'Content-Type':'application/x-www-form-urlencoded'}, data={"content": content})
-			print(resp)
+	wcb = requests.get(f'{config["fromChannel"]}?limit=50', headers=auth).json()
+	versus = requests.get(f'{config["toChannel"]}', headers=auth).json()
+
+	wcb_posts = list(map(lambda x: f'**{x["author"]["username"]}**\n{x["content"]}', wcb))
+	versus_posts = list(map(lambda x: x["content"], versus))
+	new_posts = list(set(wcb_posts) - set(versus_posts))
+
+	for x in wcb:
+		if f'**{x["author"]["username"]}**\n{x["content"]}' in new_posts:
+			content = f'**{x["author"]["username"]}**\n{x["content"]}'
+			resp = requests.post(config["webhook"], headers={'Content-Type':'application/x-www-form-urlencoded'}, data={"content": content})
+			
+			# if unauthorized get a new token and post again
+			if resp.status_code == 401:
+				refresh_token()
+				resp = requests.post(config["webhook"], headers={'Content-Type':'application/x-www-form-urlencoded'}, data={"content": content})
+			
+			print(resp.status_code)
 			time.sleep(int(1))
 
-	return len(newPosts)
+	return len(new_posts)
 
-starttime = time.time()
+def refresh_token():
+	resp = requests.post(config["authUrl"], headers={'Host': 'discord.com', 'Content-Type':'application/json'}, json={"login": config["login"], "password": config["password"]})
+	print(resp.status_code)
+	config["token"] = resp.json()["token"]
+	file = open("config.json", "w+")
+	file.write(json.dumps(config))
+	file.close()
 
-print("=========Welcome to Sun Thief 1.0===========")
+def main():
+	start_time = time.time()
+	print("=========Welcome to Sun Thief 1.0===========")
+	get_config()
 
-while True:
-	print("Starting new analysis...")
-	newPosts = sun_thief()
-	print("Analysis finished. %s new posts."%newPosts)
-	time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+	while True:
+		print("Starting new analysis...")
+		new_posts = post()
+		print("Analysis finished. %s new posts."%new_posts)
+		time.sleep(config["timer"] - ((time.time() - start_time) % config["timer"]))
+
+main()
